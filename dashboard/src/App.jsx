9c1a1939b55c46bea244f85d229fd93e294b0749
nilanjan-rgb <nilanjan@sbnri.com>
2026-02-26 00:56:31 +0530

    Revert "style: update platform logos and design polish"
    This reverts commit 8f5153a74d78936f54be95368e293daf890a2138.

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -153,8 +153,9 @@ function App() {
         switch (platform) {
             case 'Backend':
                 return (
-                    <svg viewBox="0 0 24 24" className="w-5 h-5 fill-indigo-500">
-                        <path fillRule="evenodd" d="M3 5a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2H5a2 2 0 01-2-2V5zm14 2a1 1 0 100-2 1 1 0 000 2zM5 15a2 2 0 012-2h14a2 2 0 012 2v4a2 2 0 01-2 2H5a2 2 0 01-2-2v-4zm14 2a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
+                    <svg className="w-5 h-5 text-indigo-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                     </svg>
                 );
             case 'Android':
