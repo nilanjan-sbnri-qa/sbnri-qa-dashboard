8f40412569d6921540047b3267e7cbebcdb0985c
nilanjan-rgb <nilanjan@sbnri.com>
2026-02-25 23:43:04 +0530

    add start script for render

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+    base: '/sbnri-qa-dashboard/',
     plugins: [
         react(),
         tailwindcss(),
